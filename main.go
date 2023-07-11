d2be9a9637a384dc0d9ddcc27af8b36be87ca0ca
Amir Azizafshari <55924754+amir002001@users.noreply.github.com>
2023-07-11 09:15:19 -0400

    feat: logic for creating template

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
+	"math/rand"
 	"os"
+	"time"
 
 	"github.com/charmbracelet/log"
+	"github.com/gofiber/fiber/v2"
 )
 
 type TemplatePlacehoders struct {
@@ -14,29 +18,71 @@ type TemplatePlacehoders struct {
 	Title    string
 }
 
-func main() {
-	tmplFile := "index.template.html"
-	tmpl, err := template.New(tmplFile).ParseFiles(tmplFile)
-	if err != nil {
-		log.Fatalf("nyooo \n%v", err)
-	}
+const tmplFile = "index.template.html"
 
-	f, err := os.CreateTemp("", "output-*.html")
-	if err != nil {
-		log.Fatal(err)
-	}
+func openGraphHandler(ctx *fiber.Ctx) error {
+	source := rand.NewSource(time.Now().Unix())
+	random := rand.New(source)
 
-	log.Printf("file created : %s", f.Name())
-	//	defer os.Remove(f.Name()) // clean up
+	postImageUrl := ctx.Params("image", "")
+	postTitle := ctx.Params("title", "")
+	score := random.Intn(100)
 
 	placeholders := TemplatePlacehoders{
 		Www:      "/Users/amirhosseinazizafshari/dev/og-post-space-invaders/www",
-		ImageUrl: "https://res.cloudinary.com/df3h8ffly/image/upload/v1684094745/portfolio/gallery_wp18zu.webp",
-		Score:    "12",
-		Title:    "FoodBox",
+		ImageUrl: postImageUrl,
+		Score:    fmt.Sprint(score),
+		Title:    postTitle,
 	}
-	err = tmpl.Execute(f, placeholders)
+
+	ctx.Type("image/png")
+
+	tmpHtmlFile, err := os.CreateTemp("", "output-*.html")
 	if err != nil {
-		log.Fatalf("nyooo \n%v", err)
+		return err
 	}
+	defer os.Remove(tmpHtmlFile.Name())
+
+	err = generateTemplate(tmpHtmlFile, placeholders)
+	if err != nil {
+		return err
+	}
+
+	ogImageBytes, err := generateOgImage(tmpHtmlFile)
+	if err != nil {
+		return err
+	}
+
+	if _, err := ctx.Write(ogImageBytes); err != nil {
+		return err
+	}
+	return nil
+}
+
+func generateOgImage(*os.File) ([]byte, error) {
+	return nil, nil
+}
+
+func generateTemplate(file *os.File, placeholders TemplatePlacehoders) error {
+	tmpl, err := template.New(tmplFile).ParseFiles(tmplFile)
+	if err != nil {
+		return err
+	}
+
+	err = tmpl.Execute(file, placeholders)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	app := fiber.New()
+	api := app.Group("api")
+	v1 := api.Group("v1")
+
+	v1.Get("/opengraph", openGraphHandler)
+
+	log.Fatal(app.Listen(":8080"))
 }
