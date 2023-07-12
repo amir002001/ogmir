df860127976f0a9f890cc52195171dd873ac30dc
Amir Azizafshari <55924754+amir002001@users.noreply.github.com>
2023-07-11 20:05:53 -0400

    feat: cors

--- main.go
+++ main.go
@@ -5,10 +5,14 @@ import (
 
 	"github.com/charmbracelet/log"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{AllowOrigins: "https://amir.day, *.amir002001.pages.dev"}))
+
 	api := app.Group("api")
 	v1 := api.Group("v1")
 
