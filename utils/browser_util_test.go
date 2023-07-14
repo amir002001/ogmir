2f768a973e3ed991f0f64a915a1fdf22586852fb
Amir Azizafshari <55924754+amir002001@users.noreply.github.com>
2023-07-14 09:14:12 -0400

    feat: tests

--- utils/browser_util_test.go
+++ utils/browser_util_test.go
@@ -1,1 +1,31 @@
 package utils_test
+
+import (
+	"og-post-space-invaders/utils"
+	"os"
+	"testing"
+)
+
+func TestScreenshotMainElement(t *testing.T) {
+	// Create a temporary file for testing
+	file, err := os.CreateTemp("", "testfile.*.html")
+	if err != nil {
+		t.Fatalf("Failed to create temporary file: %v", err)
+	}
+	defer os.Remove(file.Name()) // Clean up the temporary file
+
+	// Write some HTML content to the file
+	_, err = file.WriteString(`<html><body><div id="main">Hello, world!</div></body></html>`)
+	if err != nil {
+		t.Fatalf("Failed to write HTML content to the file: %v", err)
+	}
+
+	imageBytes, err := utils.ScreenshotMainElement(file)
+	if err != nil {
+		t.Fatalf("Failed to capture screenshot: %v", err)
+	}
+
+	if len(imageBytes) == 0 {
+		t.Error("Expected non-empty screenshot image, got empty")
+	}
+}
--- utils/template_util_test.go
+++ utils/template_util_test.go
@@ -1,1 +1,45 @@
 package utils_test
+
+import (
+	"html/template"
+	"og-post-space-invaders/types"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestGenerateTemplate(t *testing.T) {
+	file, err := os.CreateTemp("", "template.*.html")
+	if err != nil {
+		t.Fatalf("Failed to create temporary file: %v", err)
+	}
+	defer os.Remove(file.Name())
+
+	tmpl, err := template.New("index.template.html").ParseFiles("../index.template.html")
+	if err != nil {
+		t.Fatalf("Failed to create template from file %v", err)
+	}
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		t.Fatalf("Failed to get cwd %v", err)
+	}
+
+	wwwDir := filepath.Join(cwd, "www")
+	wwwAbs, err := filepath.Abs(wwwDir)
+	if err != nil {
+		t.Fatalf("Failed to get abs path for www folder %v", err)
+	}
+
+	placeholders := types.TemplatePlacehoders{
+		Www:      wwwAbs,
+		ImageUrl: "https://placehold.co/600x400",
+		Score:    "23",
+		Title:    "Amir",
+	}
+
+	err = tmpl.Execute(file, placeholders)
+	if err != nil {
+		t.Fatalf("Failed to generate template: %v", err)
+	}
+}
